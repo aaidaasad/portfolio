5b151bc463b11b5bf4f2a4f757bd4a0af52bb44b
aaidaasad <aaida.asad@mail.utoronto.ca>
2025-04-20 16:13:26 -0400

    push

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+import MindCraftProject from './components/projects/MindCraftProject';
+import {Route, Routes} from "react-router-dom";
 
+<<<<<<< HEAD
 // Import project pages
 import ShadowsOfJusticeProject from './components/projects/shadowsofjustice';
 import EchoesOfTheForest from './components/projects/echoesoftheforest';
 import TheLionWithin from './components/projects/thelionwithin';
 import EternalRose from './components/projects/eternalroses'; // ✅ Newly added
+=======
+>>>>>>> parent of 72cf9e2f (push)
 
 function Home() {
     return (
@@ -29,10 +33,14 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+<<<<<<< HEAD
             <Route path="/projects/shadowsofjustice" element={<ShadowsOfJusticeProject />} />
             <Route path="/projects/echoesoftheforest" element={<EchoesOfTheForest />} />
             <Route path="/projects/thelionwithin" element={<TheLionWithin />} />
             <Route path="/projects/eternalroses" element={<EternalRose />} /> 
+=======
+            <Route path="/projects/mindcraft" element={<MindCraftProject />} />
+>>>>>>> parent of 72cf9e2f (push)
         </Routes>
     );
 }
